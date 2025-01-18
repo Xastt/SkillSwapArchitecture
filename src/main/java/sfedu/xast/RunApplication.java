704c0cb0e0fb109db874f911c5327cfb2e09616e
Xast <artemhasrovyan@gmail.com>
2025-01-18 17:18:30 +0300

    Change RunApplication

--- src/main/java/sfedu/xast/RunApplication.java
+++ src/main/java/sfedu/xast/RunApplication.java
@@ -1,27 +1,60 @@
 package sfedu.xast;
 
+import com.opencsv.exceptions.CsvException;
+import org.xml.sax.SAXException;
+import sfedu.xast.api.DataProviderCsv;
+import sfedu.xast.api.DataProviderMongo;
 import sfedu.xast.api.DataProviderPSQL;
+import sfedu.xast.api.DataProviderXml;
 import sfedu.xast.models.*;
 import sfedu.xast.utils.Status;
 
+import javax.management.modelmbean.XMLParseException;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Scanner;
 
 public class RunApplication {
 
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) throws SQLException, IOException, CsvException, ParserConfigurationException, XMLParseException, SAXException {
 
+        String sourceFile = " ";
         boolean flag = true;
 
         DataProviderPSQL dataProviderPSQL;
+        DataProviderXml dataProviderXml = new DataProviderXml();
+        DataProviderMongo dataProviderMongo = new DataProviderMongo("test");
+        DataProviderCsv dataProviderCsv = new DataProviderCsv();
         Connection connection = DataProviderPSQL.getConnection();
         dataProviderPSQL = new DataProviderPSQL();
 
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Привет! Я, консольный клиент приложения SkillSwap.\n" +
-                "Сперва, давай пройдем регистрацию! \n" +
+                "Давай определимся с форматом сохранения данных. \n"+
+                "Выбери нужную цифру: \n" +
+                "1. CSV-файл\n"+
+                "2. XML-файл\n"+
+                "3. PostgreSQL\n"+
+                "4. MongoDB");
+        Integer number = sc.nextInt();
+        sc.nextLine();
+        switch (number) {
+            case 1:
+                sourceFile = "CSV";
+                break;
+            case 2:
+                sourceFile = "XML";
+                break;
+            case 3:
+                sourceFile = "PostgreSQL";
+                break;
+            case 4:
+                sourceFile = "MongoDB";
+                break;
+        }
+
+        System.out.println("Давай пройдем регистрацию! \n" +
                 "Тебе нужно будет ввести личные данные.\n" +
                 "Фамилия:");
         String surname = sc.nextLine();
@@ -32,9 +65,23 @@ public class RunApplication {
         System.out.println("Электронная почта:");
         String email = sc.nextLine();
         PersInf persInf = new PersInf(surname, name, phoneNumber, email);
-        if(dataProviderPSQL.createPersInf(persInf)){
-            System.out.println("Рад знакомству, " + persInf.getName());
-        };
+
+        switch (sourceFile){
+            case "CSV":
+                dataProviderCsv.createPersInf(persInf);
+                break;
+            case "XML":
+                dataProviderXml.createPersInf(persInf);
+                break;
+            case "PostgreSQL":
+                dataProviderPSQL.createPersInf(persInf);
+                break;
+            case "MongoDB":
+                dataProviderMongo.createPersInf(persInf);
+                break;
+        }
+
+        System.out.println("Рад знакомству, " + persInf.getName());
 
         while(flag) {
             System.out.println("Давай определимся, что ты хочешь:\n" +
@@ -50,18 +97,63 @@ public class RunApplication {
                             "Введи ее название, а я попробую найти: ");
                     String skillFind = sc.nextLine();
                     System.out.println("Вот, что мне удалось найти:");
-                    dataProviderPSQL.printProfInfList(dataProviderPSQL.readProfInfBySkillName(skillFind));
+
+                    switch (sourceFile){
+                        case "CSV":
+                            dataProviderCsv.printProfInfList(dataProviderCsv.readProfInfBySkillName(skillFind));
+                            break;
+                        case "XML":
+                            dataProviderXml.printProfInfList(dataProviderXml.readProfInfBySkillName(skillFind));
+                            break;
+                        case "PostgreSQL":
+                            dataProviderPSQL.printProfInfList(dataProviderPSQL.readProfInfBySkillName(skillFind));
+                            break;
+                        case "MongoDB":
+                            dataProviderMongo.printProfInfList(dataProviderMongo.readProfInfBySkillName(skillFind));
+                            break;
+                    }
+
                     System.out.println("Скопируй и вставь сюда ID пользователя, чей урок ты хочешь взять");
                     String neededId = sc.nextLine();
-                    ProfInf retrievedProfInf = dataProviderPSQL.readProfInfWithId(neededId);
-                    System.out.println("Отлично. Информация о уроке по навыку <" + retrievedProfInf.getSkillName() + "> направлена пользователю.\n" +
+
+                    String skillNameForAns = "";
+                    String retrievedId = "";
+                    Double retrievedRating = 0.0;
+                    switch (sourceFile){
+                        case "CSV":
+                            ProfInf retrievedProfInf = dataProviderCsv.readProfInfWithId(neededId);
+                            skillNameForAns = retrievedProfInf.getSkillName();
+                            retrievedId = retrievedProfInf.getPersId();
+                            retrievedRating = retrievedProfInf.getRating();
+                            break;
+                        case "XML":
+                            ProfInf retrievedProfInf1 = dataProviderXml.readProfInfWithId(neededId);
+                            skillNameForAns = retrievedProfInf1.getSkillName();
+                            retrievedId = retrievedProfInf1.getPersId();
+                            retrievedRating = retrievedProfInf1.getRating();
+                            break;
+                        case "PostgreSQL":
+                            ProfInf retrievedProfInf2 = dataProviderPSQL.readProfInfWithId(neededId);
+                            skillNameForAns = retrievedProfInf2.getSkillName();
+                            retrievedId = retrievedProfInf2.getPersId();
+                            retrievedRating = retrievedProfInf2.getRating();
+                            break;
+                        case "MongoDB":
+                            ProfInf retrievedProfInf3 = dataProviderMongo.readProfInfWithId(neededId);
+                            skillNameForAns = retrievedProfInf3.getSkillName();
+                            retrievedId = retrievedProfInf3.getPersId();
+                            retrievedRating = retrievedProfInf3.getRating();
+                            break;
+                    }
+
+                    System.out.println("Отлично. Информация о уроке по навыку <" + skillNameForAns + "> направлена пользователю.\n" +
                             "Ожидайте обратной связи!");
                     try {
                         Thread.sleep(6000); //тут они между собой связались, если все получилось, то даже и занятие провели
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
-                    SkillExchange skillExchange = new SkillExchange(retrievedProfInf.getSkillName(), persInf.getId(), retrievedProfInf.getPersId());
+                    SkillExchange skillExchange = new SkillExchange(skillNameForAns, persInf.getId(), retrievedId);
                     dataProviderPSQL.createSkillExchange(skillExchange);
 
                     System.out.println("Получилось ли у вас провести занятие?\n" +
@@ -76,14 +168,27 @@ public class RunApplication {
                         sc.nextLine();
                         System.out.println("Ваши впечатления: ");
                         String comment = sc.nextLine();
-                        Review review = new Review(rating, comment, persInf.getId(), retrievedProfInf.getPersId());
-                        dataProviderPSQL.insertRating(retrievedProfInf.getPersId(), review.getRating(), retrievedProfInf.getRating());
+                        Review review = new Review(rating, comment, persInf.getId(), retrievedId);
+                        dataProviderPSQL.insertRating(retrievedId, review.getRating(), retrievedRating);
                         dataProviderPSQL.createReview(review);
                         System.out.println("Нам очень приятно, что вы выбрали нашу платформу. Удачи!");
                         flag = false;
                     }else{
                         Transaction transaction = new Transaction(Status.CANCELED, skillExchange.getExchangeId());
-                        dataProviderPSQL.createTransaction(transaction);
+                        switch (sourceFile){
+                            case "CSV":
+                                dataProviderCsv.createTransaction(transaction);
+                                break;
+                            case "XML":
+                                dataProviderXml.createTransaction(transaction);
+                                break;
+                            case "PostgreSQL":
+                                dataProviderPSQL.createTransaction(transaction);
+                                break;
+                            case "MongoDB":
+                                dataProviderMongo.createTransaction(transaction);
+                                break;
+                        }
                         System.out.println("Нам очень жаль, что у вас не получилось связаться");
                         flag = false;
                     }
@@ -103,10 +208,22 @@ public class RunApplication {
                     System.out.println("Напиши свой стаж работы в этой области:");
                     Double exp = sc.nextDouble();
                     ProfInf profInf = new ProfInf(persInf.getId(),skillName,skillDescription,cost,persDescription,exp,0.0);
-                    if(dataProviderPSQL.createProfInf(profInf, persInf)){
-                        System.out.println("Отлично! Все данные записаны\n" +
-                                "Ожидайте, с вами будут связываться заинтересованные люди.");
+                    switch (sourceFile){
+                        case "CSV":
+                            dataProviderCsv.createProfInf(profInf, persInf);
+                            break;
+                        case "XML":
+                            dataProviderXml.createProfInf(profInf, persInf);
+                            break;
+                        case "PostgreSQL":
+                            dataProviderPSQL.createProfInf(profInf, persInf);
+                            break;
+                        case "MongoDB":
+                            dataProviderMongo.createProfInf(profInf, persInf);
+                            break;
                     }
+                    System.out.println("Отлично! Все данные записаны\n" +
+                                "Ожидайте, с вами будут связываться заинтересованные люди.");
                     flag = false;
                     break;
                 default:
